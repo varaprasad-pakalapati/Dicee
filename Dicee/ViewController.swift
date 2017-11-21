13d85753f5f95e99289ae95f6fdc3906b3c18adc
Pakalapati, Maha Satya (Produban) <mahasatya.pakalapati@produban.co.uk>
2017-11-21 14:22:46 +0000

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Pakalapati, Maha Satya (Produban) on 21/11/2017.
+//  Copyright © 2017 Pakalapati, Maha Satya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
